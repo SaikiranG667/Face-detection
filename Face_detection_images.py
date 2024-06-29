29f3df27677fc0115cc7d5a276b571635509d56f
SaikiranG667 <144164769+SaikiranG667@users.noreply.github.com>
2024-06-30 02:31:49 +0530

    Update Face_detection_images.py
    files

--- Face_detection_images.py
+++ Face_detection_images.py
@@ -1,12 +1,12 @@
 import cv2
 
 
-face_classifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cv2.namedWindow('image')
 img=cv2.imread('faces.png')
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-faces=face_classifier.detectMultiScale(gray,1.1,3)
+faces=face_detector.detectMultiScale(gray,1.1,3)
 for (x,y,w,h) in faces:
   cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 cv2.imshow("image",img)
